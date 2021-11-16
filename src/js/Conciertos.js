d31b9ceb0839802fcf8b2d26ecf0abea0d44215a
Jeroni <jmateo@cifpfbmoll.eu>
2021-11-16 17:47:55 +0100

    Formulario Añadir

--- src/js/Conciertos.js
+++ src/js/Conciertos.js
@@ -1,130 +1,123 @@
-
-//TODO:Objeto Conciertos
-//A continuacion creamos los objetos para los Conciertos que seran 6 en total
-export const conciertos=[
-    {
-    titulo: 'Concierto scorpions',
-    grupo:'Scorpions',
-    fecha:'24-25 Junio 2022',
-    lugar:''   
-},
-{
-    titulo: 'Muse Live Festival',
-    grupo: 'Muse',
-    fecha:'24-25-26 Junio 2022',
-    lugar:''
-},
-{
-    titlo:'FSO Fénix La Gira',
-    grupo:'Fenix',
-    fecha:'',
-    lugar:''
-},
-{
-    titlo:'Rio Babel, C. Tangana',
-    grupo:'C.Tangana',
-    fecha:'30-1-2 Julio 2022',
-    lugar:'Madrid'
-},
-{
-    titlo:'Justice World Tour, Justin Bieber',
-    grupo:'Justin Bieber',
-    fecha:'10 de Septiembre 2022',
-    lugar:'Estadio Unico de la Plata'
-},
-{
-    titlo:'Nicks Masons Saugerful of Secrets, Pink Floyd',
-    grupo:'Pink Floyd',
-    fecha:'9-10 de Julio 2022',
-    lugar:'Madrid y Barcelona'
-}
-]
-
 //TODO: Editar Eventos
 //FUncion para editar eventos
-function editEvent(){
-//Preguntamos al usuario cual quiere editar
-    var numEvent=parseInt( prompt("Que numero de concierto quieres editar"));
-//Cogemos la plantilla para editar eventos
-    var newEvent= document.getElementById("editEvent");
+function editEvent() {
+  //Preguntamos al usuario cual quiere editar
+  var numEvent = parseInt(prompt("Que numero de concierto quieres editar"));
+  //Cogemos la plantilla para editar eventos
+  var newEvent = document.getElementById("editEvent");
 
-//Dependiendo de que numero pase el por el prompt desaparecera un evento y aparecerá otro en su lugar
-    switch(numEvent){
-        case 1: 
-        document.getElementById("concierto1").style.display="none";
-        newEvent.style.display ="block";
-        break;
-        case 2:
-        document.getElementById("concierto2").style.display="none";
-        newEvent.style.display ="block";
-        break;
-        case 3:
-        document.getElementById("concierto3").style.display="none";
-        newEvent.style.display ="block";
-        break;
-        case 4:
-        document.getElementById("concierto4").style.display="none";
-        newEvent.style.display ="block";
-        break;
-        default:
-            alert("Solo hay 4 conciertos");
-            break;
-        
-    
-    }
+  //Dependiendo de que numero pase el por el prompt desaparecera un evento y aparecerá otro en su lugar
+  switch (numEvent) {
+    case 1:
+      document.getElementById("concierto1").style.display = "none";
+      newEvent.style.display = "block";
+      break;
+    case 2:
+      document.getElementById("concierto2").style.display = "none";
+      newEvent.style.display = "block";
+      break;
+    case 3:
+      document.getElementById("concierto3").style.display = "none";
+      newEvent.style.display = "block";
+      break;
+    case 4:
+      document.getElementById("concierto4").style.display = "none";
+      newEvent.style.display = "block";
+      break;
+    default:
+      alert("Solo hay 4 conciertos");
+      break;
+  }
 }
-
 
 //TODO: Eliminar Eventos
 //Funcion para eliminar Eventos
 function eliminarEvent(){
-//Preguntamos al usuario cual quiere eliminar
+  //Preguntamos al usuario cual quiere eliminar
 
-    var numEvent=parseInt(prompt("Que numero de concierto quieres eliminar"));
+  var numEvent = parseInt(prompt("Que numero de concierto quieres eliminar"));
 
-    //Dependiendo de que numero pase el por el prompt desaparecera un evento u otro
-switch(numEvent){
-    case 1: 
-    document.getElementById("concierto1").style.display="none";
-    break;
+  //Dependiendo de que numero pase el por el prompt desaparecera un evento u otro
+  switch (numEvent) {
+    case 1:
+      document.getElementById("concierto1").style.display = "none";
+      break;
     case 2:
-    document.getElementById("concierto2").style.display="none";
-    break;
+      document.getElementById("concierto2").style.display = "none";
+      break;
     case 3:
-    document.getElementById("concierto3").style.display="none";
-    break;
+      document.getElementById("concierto3").style.display = "none";
+      break;
     case 4:
-    document.getElementById("concierto4").style.display="none";
-    break;
+      document.getElementById("concierto4").style.display = "none";
+      break;
     case 5:
-        document.getElementById("concierto5").style.display="none";
-        break;
+      document.getElementById("concierto5").style.display = "none";
+      break;
     case 6:
-         document.getElementById("concierto6").style.display="none";
-         break;
+      document.getElementById("concierto6").style.display = "none";
+      break;
     default:
-        alert("Solo hay 6 conciertos");
-        break;
-    
-
-}
+      alert("Solo hay 6 conciertos");
+      break;
+  }
 }
 
-
-//TODO:Formulario Busqueda
+//TODO:Busqueda Formulario 
 //Cuando pulsamos el boton de Grupos o estilos salta el formulario
-      function mostrarFormulario(){
-        var formulario = document.getElementById("formularioBusqueda");
-        formulario.style.display = block;
-        var evento = document.getElementById("secciones");
-        evento.style.display= block;
-        }
+function mostrarFormulario() {
+    document.getElementById("formularioBusqueda").style.display = block;
+    document.getElementById("secciones").style.display = block;
+}
+//TODO: Nuevo Concierto Formulario
+function añadirConcierto(){
+    document.getElementById("formularioNew").style.display = "block";
+}
 
 //TODO:DATAPICKER para elegir fecha
 //Datapicker
-$( function() {
-    $( "#datepicker" ).datepicker();
-  } );
+$(function () {
+  $("#datepicker").datepicker();
+});
 
-
-
+//TODO:Objeto Conciertos
+//A continuacion creamos los objetos para los Conciertos que seran 6 en total
+export const conciertos = [
+    {
+      titulo: "Concierto scorpions",
+      grupo: "Scorpions",
+      fecha: "24-25 Junio 2022",
+      lugar: "",
+    },
+    {
+      titulo: "Muse Live Festival",
+      grupo: "Muse",
+      fecha: "24-25-26 Junio 2022",
+      lugar: "",
+    },
+    {
+      titlo: "FSO Fénix La Gira",
+      grupo: "Fenix",
+      fecha: "",
+      lugar: "",
+    },
+    {
+      titlo: "Rio Babel, C. Tangana",
+      grupo: "C.Tangana",
+      fecha: "30-1-2 Julio 2022",
+      lugar: "Madrid",
+    },
+    {
+      titlo: "Justice World Tour, Justin Bieber",
+      grupo: "Justin Bieber",
+      fecha: "10 de Septiembre 2022",
+      lugar: "Estadio Unico de la Plata",
+    },
+    {
+      titlo: "Nicks Masons Saugerful of Secrets, Pink Floyd",
+      grupo: "Pink Floyd",
+      fecha: "9-10 de Julio 2022",
+      lugar: "Madrid y Barcelona",
+    },
+  ];
+  
--- src/js/Inicio.js
+++ src/js/Inicio.js
@@ -1,74 +1,85 @@
-
 //TODO:Objeto News
-export const News=[
-    {
-     titular: 'El festival Iruña Rock 2022 anuncia su cartel completo',
-     descripcion: 'El festival Iruña Rock ha anunciado el cartel completo de su edición 2022, que tendrá lugar en el Navarra Arena los días 18 y 19 de marzo'
-    },
-    {
-        titular: 'El Let\'s Festival anuncia sus primeros nombres para 2022',
-        descripcion: 'Pero las cosas van a ser diferentes en 2022, con un festival que volverá a pleno rendimiento y que, para empezar'
-       },
-       {
-        titular: 'Los festivales vuelven con fuerza en 2022',
-        descripcion: 'Festivales 2022. Ultra Miami 2022. Ultra Miami es históricamente el punto de inicio de la temporada de grandes festivales.'
-       },
-       {
-        titular: 'Festival Actual 2022 en Logroño: Conciertos y Entradas',
-        descripcion: 'Festival Actual 2022 regresará a la localidad riojana del 2 al 8 de enero del próximo año'
-       },
-       {
-        titular: 'Los festivales de 2022 recibirán en subvenciones un 72% más que este año',
-        descripcion: 'ya han abierto agenda y hacen números para organizar los festivales del 2022.'
-       },
-       {
-        titular: 'Alizzz, el primer artista confirmado para el festival Brizna 2022',
-        descripcion: 'Alizzz es el primer artista confirmado para el festival Brizna 2022. Uno de los productores de pop español más importante e influyente '
-       },
-       {
-        titular: 'Festival de Lucerna 2022: Diversidad',
-        descripcion: 'El Festival de Lucerna comparte su programación para el verano de 2022. Anne-Sophie Mutter, Lang Lang, Daniel Barenboim, Sir Simon Rattle,...'
-       },
-       {
-        titular: 'El Festival de Les Arts anuncia fechas en 2022',
-        descripcion: 'Para 2022, el festival valenciano de referencia volverá a llenar de música la Ciudad de las Artes y las Ciencias durante los días 3 y 4'
-       },
-       {
-        titular: 'El Festival de Les Arts anuncia fechas en 2022',
-        descripcion: 'El festival Palencia Sonora, que volverá a su formato habitual entre los días 10 y 12 de junio de 2022, ha ampliado su cartel con Alizzz'
-       },
-       {
-        titular: 'Cáceres creará en 2022 una marca para "proteger" los festivales consolidados y garantizar su financiación',
-        descripcion: 'El alcalde de Cáceres, Luis Salaya, ha anunciado que en 2022 se creará una marca local para ejercer de paraguas de protección de los festivales'
-       }
-]
-
+export const News = [
+  {
+    titular: "El festival Iruña Rock 2022 anuncia su cartel completo",
+    descripcion:
+      "El festival Iruña Rock ha anunciado el cartel completo de su edición 2022, que tendrá lugar en el Navarra Arena los días 18 y 19 de marzo",
+  },
+  {
+    titular: "El Let's Festival anuncia sus primeros nombres para 2022",
+    descripcion:
+      "Pero las cosas van a ser diferentes en 2022, con un festival que volverá a pleno rendimiento y que, para empezar",
+  },
+  {
+    titular: "Los festivales vuelven con fuerza en 2022",
+    descripcion:
+      "Festivales 2022. Ultra Miami 2022. Ultra Miami es históricamente el punto de inicio de la temporada de grandes festivales.",
+  },
+  {
+    titular: "Festival Actual 2022 en Logroño: Conciertos y Entradas",
+    descripcion:
+      "Festival Actual 2022 regresará a la localidad riojana del 2 al 8 de enero del próximo año",
+  },
+  {
+    titular:
+      "Los festivales de 2022 recibirán en subvenciones un 72% más que este año",
+    descripcion:
+      "ya han abierto agenda y hacen números para organizar los festivales del 2022.",
+  },
+  {
+    titular:
+      "Alizzz, el primer artista confirmado para el festival Brizna 2022",
+    descripcion:
+      "Alizzz es el primer artista confirmado para el festival Brizna 2022. Uno de los productores de pop español más importante e influyente ",
+  },
+  {
+    titular: "Festival de Lucerna 2022: Diversidad",
+    descripcion:
+      "El Festival de Lucerna comparte su programación para el verano de 2022. Anne-Sophie Mutter, Lang Lang, Daniel Barenboim, Sir Simon Rattle,...",
+  },
+  {
+    titular: "El Festival de Les Arts anuncia fechas en 2022",
+    descripcion:
+      "Para 2022, el festival valenciano de referencia volverá a llenar de música la Ciudad de las Artes y las Ciencias durante los días 3 y 4",
+  },
+  {
+    titular: "El Festival de Les Arts anuncia fechas en 2022",
+    descripcion:
+      "El festival Palencia Sonora, que volverá a su formato habitual entre los días 10 y 12 de junio de 2022, ha ampliado su cartel con Alizzz",
+  },
+  {
+    titular:
+      'Cáceres creará en 2022 una marca para "proteger" los festivales consolidados y garantizar su financiación',
+    descripcion:
+      "El alcalde de Cáceres, Luis Salaya, ha anunciado que en 2022 se creará una marca local para ejercer de paraguas de protección de los festivales",
+  },
+];
 
 //TODO:Objeto principal Events
-export const principalEvents=[
-    {
-        nombre:'Mallorca Live Festival',
-        descripcion:'Venid al gran festival de la musica en Mallorca'
-    },
-    {
-        nombre:'The BPM Festival: Ibiza Showcases',
-        descripcion:'Gran Festival de BPM en Ibiza'
-    },
-    {
-        nombre:'Madrid Popfest 2022',
-        descripcion:'Buena fiesta del Mejor Pop en Madrid'
-    }
-]
+export const principalEvents = [
+  {
+    nombre: "Mallorca Live Festival",
+    descripcion: "Venid al gran festival de la musica en Mallorca",
+  },
+  {
+    nombre: "The BPM Festival: Ibiza Showcases",
+    descripcion: "Gran Festival de BPM en Ibiza",
+  },
+  {
+    nombre: "Madrid Popfest 2022",
+    descripcion: "Buena fiesta del Mejor Pop en Madrid",
+  },
+];
 
-//TODO:Formulario de Busqueda 
-function mostrarFormulario(){
-    var formulario = document.getElementById("formulario");
-    formulario.style.display = block;
-    var evento = document.getElementById("eventos");
-    evento.style.display= block;
+//TODO:Formulario de Busqueda
+function mostrarFormulario() {
+  var formulario = document.getElementById("formulario");
+  formulario.style.display = block;
+  var evento = document.getElementById("eventos");
+  evento.style.display = block;
 }
 
 //Datapicker
-$( function() {
-    $( "#datepicker" ).datepicker();
-  } );
+$(function () {
+  $("#datepicker").datepicker();
+});
