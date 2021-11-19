7e2f3498540206822696553f7b447be62e86b801
Daniel Maestre <dmaestre@cifpfbmoll.eu>
2021-11-19 17:29:41 +0100

    Botón go back to top (y cambios menores)

--- src/js/Inicio.js
+++ src/js/Inicio.js
@@ -3,52 +3,102 @@ const News = {
     noticias: [{
             foto: '<img src="images/noticias.png" alt="No se pudo mostrar la imagen en tu navegador"></img>',
             titular: 'El festival Iruña Rock 2022 anuncia su cartel completo',
-            descripcion: 'El festival Iruña Rock ha anunciado el cartel completo de su edición 2022, que tendrá lugar en el Navarra Arena los días 18 y 19 de marzo.'
+            descripcion: 'El festival Iruña Rock ha anunciado el cartel completo de su edición 2022, que tendrá lugar en el Navarra Arena los días 18 y 19 de marzo.',
+            fecha: {
+                dia: 21,
+                mes: 10,
+                anyo: 2021
+            }
         },
         {
             foto: '<img src="images/noticias.png" alt="No se pudo mostrar la imagen en tu navegador"></img>',
             titular: 'El Let\'s Festival anuncia sus primeros nombres para 2022',
-            descripcion: 'Pero las cosas van a ser diferentes en 2022, con un festival que volverá a pleno rendimiento.'
+            descripcion: 'Pero las cosas van a ser diferentes en 2022, con un festival que volverá a pleno rendimiento.',
+            fecha: {
+                dia: 11,
+                mes: 9,
+                anyo: 2021
+            }
         },
         {
             foto: '<img src="images/noticias.png" alt="No se pudo mostrar la imagen en tu navegador"></img>',
             titular: 'Los festivales vuelven con fuerza en 2022',
-            descripcion: 'Festivales 2022. Ultra Miami 2022. Ultra Miami es históricamente el punto de inicio de la temporada de grandes festivales.'
+            descripcion: 'Festivales 2022. Ultra Miami 2022. Ultra Miami es históricamente el punto de inicio de la temporada de grandes festivales.',
+            fecha: {
+                dia: 27,
+                mes: 10,
+                anyo: 2021
+            }
         },
         {
             foto: '<img src="images/noticias.png" alt="No se pudo mostrar la imagen en tu navegador"></img>',
             titular: 'Festival Actual 2022 en Logroño: Conciertos y Entradas',
-            descripcion: 'Festival Actual 2022 regresará a la localidad riojana del 2 al 8 de enero del próximo año.'
+            descripcion: 'Festival Actual 2022 regresará a la localidad riojana del 2 al 8 de enero del próximo año.',
+            fecha: {
+                dia: 3,
+                mes: 8,
+                anyo: 2021
+            }
         },
         {
             foto: '<img src="images/noticias.png" alt="No se pudo mostrar la imagen en tu navegador"></img>',
             titular: 'Los festivales de 2022 recibirán en subvenciones un 72% más que este año',
-            descripcion: 'Ya han abierto agenda y hacen números para organizar los festivales del 2022.'
+            descripcion: 'Ya han abierto agenda y hacen números para organizar los festivales del 2022.',
+            fecha: {
+                dia: 15,
+                mes: 9,
+                anyo: 2021
+            }
         },
         {
             foto: '<img src="images/noticias.png" alt="No se pudo mostrar la imagen en tu navegador"></img>',
             titular: 'Alizzz, el primer artista confirmado para el festival Brizna 2022',
-            descripcion: 'Alizzz es el primer artista confirmado para el festival Brizna 2022. Uno de los productores de pop español más importante e influyente.'
+            descripcion: 'Alizzz es el primer artista confirmado para el festival Brizna 2022. Uno de los productores de pop español más importante e influyente.',
+            fecha: {
+                dia: 7,
+                mes: 7,
+                anyo: 2021
+            }
         },
         {
             foto: '<img src="images/noticias.png" alt="No se pudo mostrar la imagen en tu navegador"></img>',
             titular: 'Festival de Lucerna 2022: Diversidad',
-            descripcion: 'El Festival de Lucerna comparte su programación para el verano de 2022. Anne-Sophie Mutter, Lang Lang, Daniel Barenboim, Sir Simon Rattle,...'
+            descripcion: 'El Festival de Lucerna comparte su programación para el verano de 2022. Anne-Sophie Mutter, Lang Lang, Daniel Barenboim, Sir Simon Rattle,...',
+            fecha: {
+                dia: 29,
+                mes: 10,
+                anyo: 2021
+            }
         },
         {
             foto: '<img src="images/noticias.png" alt="No se pudo mostrar la imagen en tu navegador"></img>',
             titular: 'El Festival de Les Arts anuncia fechas en 2022',
-            descripcion: 'Para 2022, el festival valenciano de referencia volverá a llenar de música la Ciudad de las Artes y las Ciencias durante los días 3 y 4.'
+            descripcion: 'Para 2022, el festival valenciano de referencia volverá a llenar de música la Ciudad de las Artes y las Ciencias durante los días 3 y 4.',
+            fecha: {
+                dia: 5,
+                mes: 11,
+                anyo: 2021
+            }
         },
         {
             foto: '<img src="images/noticias.png" alt="No se pudo mostrar la imagen en tu navegador"></img>',
             titular: 'El Festival de Les Arts anuncia fechas en 2022',
-            descripcion: 'El festival Palencia Sonora, que volverá a su formato habitual entre los días 10 y 12 de junio de 2022, ha ampliado su cartel con Alizzz.'
+            descripcion: 'El festival Palencia Sonora, que volverá a su formato habitual entre los días 10 y 12 de junio de 2022, ha ampliado su cartel con Alizzz.',
+            fecha: {
+                dia: 24,
+                mes: 10,
+                anyo: 2021
+            }
         },
         {
             foto: '<img src="images/noticias.png" alt="No se pudo mostrar la imagen en tu navegador"></img>',
             titular: 'Cáceres creará en 2022 una marca para "proteger" los festivales consolidados y garantizar su financiación',
-            descripcion: 'El alcalde de Cáceres, Luis Salaya, ha anunciado que en 2022 se creará una marca local para ejercer de paraguas de protección de los festivales.'
+            descripcion: 'El alcalde de Cáceres, Luis Salaya, ha anunciado que en 2022 se creará una marca local para ejercer de paraguas de protección de los festivales.',
+            fecha: {
+                dia: 3,
+                mes: 11,
+                anyo: 2021
+            }
         }
     ]
 }
@@ -95,6 +145,8 @@ function imprimirNews() {
         txt += '<div class="info">';
         txt += "<h4>" + News.noticias[i].titular + "</h4>";
         txt += "<p>" + News.noticias[i].descripcion + "</p>";
+        txt += `<p>Fecha publicación: 
+        ${News.noticias[i].fecha.dia}/${News.noticias[i].fecha.mes}/${News.noticias[i].fecha.anyo}</p>`;
         txt += '<button id="editNot"><i class="fas fa-pencil-alt"></i></button>';
         txt += '<button class="delNot"><i class="fas fa-trash-alt"></i></button>';
         txt += '</div></div>'
@@ -121,3 +173,25 @@ imprimirEventos();
 /* document.getElementById("follow").addEventListener("click", function () {
     imprimirNews();
 }); */
+
+//Get the button:
+buttop = document.getElementById("top");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        buttop.style.display = "block";
+    } else {
+        buttop.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function backToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
